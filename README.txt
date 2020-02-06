5f27b69f319bd992c0fa322d7235670c27242a88
smithjere14 <smithjere14@students.ecu.edu>
2020-02-06 15:52:15 -0500

    created requirements.txt updated the README.txt with directions

--- README.txt
+++ README.txt
@@ -1,1 +1,20 @@
 README
+AUTHOR: Jeremiah D Smith
+
+HOW TO SETUP AND RUN TOKENIZER.PY:
+* Used Python 3.8.1 Might be ok with lesser Python 3.x version
+01. Create a virtual environment and activate it
+        Follow the directions in the link if you need help creating and activating a virtual environment
+        https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/
+02. Install Git
+03. Issue the following command at your terminal in your virtual environment
+        git clone https://github.com/smithjere14/Info_Retrieval_HW01.git
+04. After the repository is installed issue the following command to install any necessary libraries
+        pip install -r requirements.txt
+05. Type tokenizer.py and hit Enter (You'll have to wait a minute or two depending on your computer but it works)
+
+
+
+
+
+
--- tokenizer.py
+++ tokenizer.py
@@ -1,12 +1,21 @@
+"""
+Author:
+    * Jeremiah D 'JD' Smith
 
+Description:
+    * Takes in texts and breaks them down to tokens/words
+    * Then counts the frequency of the tokens/words
+"""
 import os
 import operator
 # imported because I can't use java based Porter stemmer
 from nltk.stem import PorterStemmer
 
+# TODO: Reformat entire code into functions and modules
 # create list of stopwords
 stop_list = []
 # convert stopwords text to list
+# TODO: ask for file of stopwords
 with open('stopwords.txt', 'r') as stop_words:
     for entry in stop_words:
         stop_list.append(entry[:-1])
@@ -19,6 +28,7 @@ word = []
 word_list = {}
 porter = PorterStemmer()
 
+# TODO: ask for directory of text(s)
 # opening files in directory citeseer/
 with os.scandir('citeseer') as filenames:
     for file in filenames:
@@ -37,17 +47,22 @@ with os.scandir('citeseer') as filenames:
                             current_word = ''.join(word)
                             # clearing word list to create new word
                             word.clear()
-
+                            # filters stopwords
                             if current_word not in stop_list:
+                                # stems the token
                                 current_word = porter.stem(current_word)
                                 if current_word not in word_list:
+                                    # initializes a dictionary entry
                                     word_list[current_word] = 1
                                 else:
+                                    # adds to the frequency of the dictionary \
+                                    # entry
                                     word_list[current_word] += 1
             f.close()
 
-sorted_dictionary = dict(sorted(word_list.items(), key=operator.itemgetter(1),
-                                reverse=True))
+# sorts word_list by value as a list of tuples and types as a dictionary
+sorted_dictionary = dict(sorted(word_list.items(), key=operator.itemgetter(1)))
+                                # , reverse=True))  # show in ascending order
 sorted_dictionary.pop('')
 for keys, values in sorted_dictionary.items():
     print(keys, values)
